2a9d624c5ce00192bf3daa3696c5bb2601e8e95e
mansoorelahi <mansoorelahi777@gmail.com>
2017-05-24 01:39:08 +0530

    use strong params

--- app/controllers/seo_tags_controller.rb
+++ app/controllers/seo_tags_controller.rb
@@ -4,8 +4,8 @@ class SeoTagsController < ApplicationController
   # creates the seo tags for given path (params[:seo_tag][:path])
   def create
     if params[:key] && params[:key] == authentication_key
-      @tag = SeoTag.find_or_create_by_path(params[:seo_tag][:path])
-      @tag.update_attributes(params[:seo_tag])
+      @tag = SeoTag.find_or_create_by_path(seo_tag_params[:path])
+      @tag.update_attributes(seo_tag_params)
       respond_to do |format|
         format.js
         format.html {redirect_to params[:path]}
@@ -17,8 +17,8 @@ class SeoTagsController < ApplicationController
   # updates the seo tags for given path (params[:seo_tag][:path])
   def update
     if params[:key] && params[:key] == authentication_key
-      @tag = SeoTag.find_or_create_by_path(params[:seo_tag][:path])
-      @tag.update_attributes(params[:seo_tag])
+      @tag = SeoTag.find_or_create_by_path(seo_tag_params[:path])
+      @tag.update_attributes(seo_tag_params)
       respond_to do |format|
         format.js
         format.html {redirect_to params[:path]}
@@ -27,6 +27,11 @@ class SeoTagsController < ApplicationController
   end
   
 private
+
+  def seo_tag_params
+    params.require(:seo_tag).permit(:title, :description, :keywords, :canonical, :open_graph,
+     :path, :use_project_title, :canonical, :nofollow, :noindex)
+  end
 
   # Reads the authentication key from the seo_fuel_settings.yml file
   def authentication_key
--- app/models/seo_tag.rb
+++ app/models/seo_tag.rb
@@ -1,5 +1,4 @@
 class SeoTag < ActiveRecord::Base
-  attr_accessible :title, :description, :keywords, :canonical, :open_graph, :path, :use_project_title, :canonical, :nofollow, :noindex
   serialize :open_graph
   
   def show_title
